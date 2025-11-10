020252a461678f80cd1a7c0bd975f2fc5d74b150
DawidBeres <dawidkonsola@gmail.com>
2025-11-10 09:10:01 +0000

    Lab-08 Vue implementation 03

--- src/main/kotlin/ie/setu/config/DBConfig.kt
+++ src/main/kotlin/ie/setu/config/DBConfig.kt
@@ -1,8 +1,13 @@
 package ie.setu.config
 
+import ie.setu.domain.User
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.jetbrains.exposed.sql.Database
 import org.postgresql.util.PSQLException
+import ie.setu.domain.db.Activities
+import ie.setu.domain.db.Users
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
 class DBConfig {
 
@@ -11,26 +16,53 @@ class DBConfig {
 
     fun getDbConnection(): Database {
 
-        val PGHOST = System.getenv("POSTGRESQL_HOST") ?: "localhost"
+        val rawHost = System.getenv("POSTGRESQL_HOST") ?: "localhost"
+        val PGHOST = rawHost.substringAfterLast("://")
         val PGPORT = System.getenv("POSTGRESQL_SERVICE_PORT") ?: "5432"
-        val PGDATABASE = System.getenv("POSTGRESQL_DATABASE") ?: "defaultdb"
-        val PGUSER = System.getenv("POSTGRESQL_USER") ?: "defaultuser"
-        val PGPASSWORD = System.getenv("POSTGRESQL_PASSWORD") ?: "defaultpass"
+        val PGDATABASE = System.getenv("POSTGRESQL_DATABASE") ?: ""
+        val PGUSER = System.getenv("POSTGRESQL_USER") ?: "sa"
+        val PGPASSWORD = System.getenv("POSTGRESQL_PASSWORD") ?: ""
 
-        val dbUrl = "jdbc:postgresql://$PGHOST:$PGPORT/$PGDATABASE"
+        //val dbUrl = "jdbc:postgresql://$PGHOST:$PGPORT/$PGDATABASE"
 
         try {
-            logger.info { "Starting DB Connection...\nURL: $dbUrl" }
-            dbConfig = Database.connect(
-                url = dbUrl,
-                driver = "org.postgresql.Driver",
-                user = PGUSER,
-                password = PGPASSWORD
-            )
-            logger.info { "DB Connected Successfully to $PGDATABASE at $PGHOST:$PGPORT" }
+            logger.info { "Starting DB Connection...\nPGHOST: $PGHOST" }
+
+            if (PGHOST == "localhost") {
+                logger.info { "Using local h2 instance for development." }
+                dbConfig = Database.connect(
+                    url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
+                    driver = "org.h2.Driver",
+                    user = PGUSER,
+                    password = PGPASSWORD
+                )
+                transaction {
+                    SchemaUtils.create(Users)
+                    SchemaUtils.create(Activities)
+                }
+            } else {
+                logger.info { "Using remote PostgreSQL instance" }
+
+                val dbUrl = "jdbc:postgresql://$rawHost:$PGPORT/$PGDATABASE"
+
+                dbConfig = Database.connect(
+                    url = dbUrl,
+                    driver = "org.postgresql.Driver",
+                    user = PGUSER,
+                    password = PGPASSWORD
+                )
+            }
+
+            transaction {
+                exec("SELECT 1;")
+            }
+            logger.info { "DB Connection successfully started to $PGDATABASE at $rawHost:$PGPORT" }
+
         } catch (e: PSQLException) {
             logger.error(e) { "Error in DB connection: ${e.message}" }
+            logger.info { "Env vars used: host=$PGHOST, port=$PGPORT, user=$PGUSER, db=$PGDATABASE" }
         }
+
         return dbConfig
     }
 }
---
+++ src/main/resources/vue/views/home-page.vue
@@ -0,0 +1,10 @@
+<template id="home-page">
+  <h1> Welcome to Health Tracker App</h1>
+</template>
+
+<script>
+app.component('home-page',
+    {
+      template: "#home-page",
+    });
+</script>
